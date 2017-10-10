7d90d79005bda3e38a4f2e2958bb40d223a120e1
Luke Finney <lucaswfinney@gmail.com>
2017-10-09 19:33:40 -0600

    add css preprocessor

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="side-bar">
           <h1 className="App-title">A long time ago, in a galaxy far, far away...</h1>
         </header>
-        <main>
+        <main className="content-container">
+          <h1 className="title">SWapi-Box</h1>
           Meesa want to make web app, okey-tay?
         </main>
       </div>
