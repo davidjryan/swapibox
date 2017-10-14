e2231fa848fed9c13b5166edf133f2684690b4f4
Luke Finney <lucaswfinney@gmail.com>
2017-10-14 12:26:06 -0600

    delete commented out code

--- src/helper.js
+++ src/helper.js
@@ -44,37 +44,4 @@ export default function dataCleaner(dataObject) {
       })
     );
   }
-  // const people = dataObject[1].map( peopleData =>
-  //   Object.assign({
-  //     Name: peopleData.name,
-  //     Homeworld: peopleData.homeworld,
-  //     Species: peopleData.species[0].name,
-  //     Language: peopleData.species[0].language,
-  //     "Homeworld Population": peopleData.homeworldPopulation,
-  //     favorite: false
-  //   }));
-  //
-  // const planets = dataObject[2].map( planetData =>
-  //   Object.assign({
-  //     Name: planetData.name,
-  //     Terrain: planetData.terrain,
-  //     Population: planetData.population,
-  //     Climate: planetData.climate,
-  //     Residents: planetData.residents.map(resident => resident.name),
-  //     favorite: false
-  //   }));
-  //
-  // const vehicles = dataObject[3].map( vehicleData =>
-  //   Object.assign({
-  //     Name: vehicleData.name,
-  //     Model: vehicleData.model,
-  //     Class: vehicleData.vehicle_class,
-  //     Passengers: vehicleData.passengers,
-  //     favorite: false
-  //   }));
-  //
-  //
-  // return ([films, people, planets, vehicles]);
 }
-
-// export default
