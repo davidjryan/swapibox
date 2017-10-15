f5153b3c22d79d223b7db3a7c1e086efd3f2416f
Luke Finney <lucaswfinney@gmail.com>
2017-10-15 15:37:02 -0600

    Final refactor

---
+++ src/MockData/filmMockApp.js
@@ -0,0 +1,46 @@
+export default {
+  results: [
+    {
+      opening_crawl: "A New Hope crawl",
+      episode_id: 4,
+      release_date: "1977-05-25",
+      title: "A New Hope"
+    },
+    {
+      opening_crawl: "Attack of the Clones crawl",
+      episode_id: 2,
+      release_date: "2002-05-16",
+      title: "Attack of the Clones"
+    },
+    {
+      opening_crawl: "The Phantom Menace crawl",
+      episode_id: 1,
+      release_date: "1999-05-19",
+      title: "The Phantom Menace"
+    },
+    {
+      opening_crawl: "Revenge of the Sith crawl",
+      episode_id: 3,
+      release_date: "2005-05-19",
+      title: "Revenge of the Sith"
+    },
+    {
+      opening_crawl: "Return of the Jedi crawl",
+      episode_id: 6,
+      release_date: "1983-05-25",
+      title: "Return of the Jedi"
+    },
+    {
+      opening_crawl: "The Empire Strikes Back crawl",
+      episode_id: 5,
+      release_date: "1980-05-17",
+      title: "The Empire Strikes Back"
+    },
+    {
+      opening_crawl: "The Force Awakens crawl",
+      episode_id: 7,
+      release_date: "2015-12-11",
+      title: "The Force Awakens"
+    }
+  ]
+};
--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import fetchMock from 'fetch-mock';
+import filmMockApp from '../MockData/filmMockApp';
 import filmMock from '../MockData/filmMock';
+import vehicleMockApp from '../MockData/vehicleMockApp';
 import vehicleMock from '../MockData/vehicleMock';
 import App from '../Components/App/App';
 import dataCleaner from '../helper';
 
 describe('App', () => {
   let wrapper;
-  let filmMock;
+  const cleanedFilmData = dataCleaner(filmMock);
+  const cleanedVehicleData = dataCleaner(vehicleMock);
 
   const pause = () => {
     return new Promise(res => {
@@ -18,23 +21,66 @@ describe('App', () => {
     });
   };
 
-  it('Sets state with data after component mounts', async () => {
+  beforeEach( async () => {
     fetchMock.get('https://swapi.co/api/films', {
       status: 200,
-      body: JSON.stringify(filmMock)
+      body: filmMockApp
     });
     wrapper = mount(<App />);
     await pause();
     wrapper.setState({
-      filmData: dataCleaner(filmMock)
+      filmData: cleanedFilmData
     });
   });
 
-  // it('should not display its card container components by default',
-      // () => {
-  //   const wrapper = shallow(<App />)
-  //
-  //   expect(wrapper.find('CardContainer').length).toEqual(0)
-  //
-  // });
+  it('Sets state with data after component mounts', async () => {
+    expect(wrapper.state().filmData).toEqual(cleanedFilmData);
+  });
+
+  it('Loads data and builds cards after user cilck', async () => {
+    const vehicleButton = wrapper.find('.vehicles-btn');
+
+    expect(wrapper.find('Card').length).toEqual(0);
+    fetchMock.get('https://swapi.co/api/vehicles', {
+      status: 200,
+      body: vehicleMockApp
+    });
+
+    vehicleButton.simulate('click');
+    await pause();
+    wrapper.setState({
+      displayData: 'vehicles',
+      vehicleData: cleanedVehicleData
+    });
+
+    expect(wrapper.find('Card').length).toEqual(3);
+
+  });
+
+  it('Should allow user to favorite cards and display favorites', async () => {
+    const vehicleButton = wrapper.find('.vehicles-btn');
+    const favoriteButton = wrapper.find('.favorites-btn');
+
+    expect(wrapper.find('Card').length).toEqual(0);
+    fetchMock.get('https://swapi.co/api/vehicles', {
+      status: 200,
+      body: vehicleMockApp
+    });
+
+    vehicleButton.simulate('click');
+    await pause();
+    wrapper.setState({
+      displayData: 'vehicles',
+      vehicleData: cleanedVehicleData
+    });
+
+    const cardFavButton = wrapper.find('.favorite-button').first();
+
+    expect(wrapper.state().favoriteCards.length).toEqual(0);
+    cardFavButton.simulate('click');
+    expect(wrapper.state().favoriteCards.length).toEqual(1);
+
+    favoriteButton.simulate('click');
+    expect(wrapper.find('Card').length).toEqual(1);
+  });
 });
