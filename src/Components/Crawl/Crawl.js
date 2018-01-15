25c04eec0eabd07cba3922ccebdbaa45f32c33cb
Luke Finney <lucaswfinney@gmail.com>
2018-01-15 16:23:52 -0700

    Fix crawl text

--- src/Components/Crawl/Crawl.js
+++ src/Components/Crawl/Crawl.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Crawl = ({ films }) => {
-  const randomNumber = Math.floor(Math.random() * 8) + 1;
+  const randomNumber = Math.floor(Math.random() * 7) + 1;
   const movieCrawl = films.find( (film) => {
     return film.episodeNum === randomNumber;
   });
   return (
     <div className="crawl-container">
-      <aside className="side-bar">
+      <aside className="crawl-flex">
         <div className="crawl">
           <h2 className="crawl-title">{movieCrawl.title}</h2>
           <h2 className="crawl-title">{movieCrawl.releaseYear}</h2>
